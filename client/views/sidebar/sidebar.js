5fdeccb7421c0b962c79fbe4f43aa182117f9113
Sean <seanokeefe0314@gmail.com>
2014-05-26 12:52:42 -0700

    updates

--- client/views/sidebar/sidebar.js
+++ client/views/sidebar/sidebar.js
@@ -1,14 +1,10 @@
 Template.navigation.rendered = function(){
 
-	// $('#sidebar').first()
-	//   .sidebar('attach events', '.toggle.button')
-	// ;
-	// $('.toggle.button')
-	//   .removeClass('disabled')
-	// ;
-
-
    $('.ui.sidebar')
       .sidebar('attach events', '.launch.button')
     ;
+
+    // $('.ui.sidebar')
+    //   .dropdown('attach events', '.launch.button')
+    // ;
 }
